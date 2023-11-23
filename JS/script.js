cec7e8a3b32e8aebf316dc9adb166c5470d6efff
Shubham Anand <shubhamanand10122003@gmail.com>
2023-11-23 16:13:08 +0530

    Little update

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,35 @@
-var s = 4;
+        // let a = prompt("Enter a: ");
+        // let b = prompt("Enter b: ");
+        // let c = prompt("Enter c: ");
+        // let d = prompt("Enter d: ");
+        // let com = prompt("Enter Comparison value: ");
 
-for (var i = 1; i <= 10; i++) 
-{
-    console.log(s + " * " + i + " = " + (i * s));
-}
+        // if (com <= 5) {
+        //     console.log("Value is: " + (a + b - (c / d) * a))
+
+        // }
+        // else {
+        //     console.log("Value is: " + (a - b + (c * d) / a))
+        // }
+
+        // alert(p);
+
+        // for (var i = 1; i <= 10; i++) {
+        //     console.log(p + " * " + i + " = " + (i * p));
+        // }
+
+
+        const statement = {
+            FirstName: "Shubham",
+            LastName: "Anand",
+            RollNo: "2200290130157",
+            Bracnh: "IT",
+            EmailID: "shubhamanand10122003@gmail.com"
+        };
+
+        // document.getElementById("ab").innerHTML = statement.FirstName;
+        console.log(document.getElementById("ab").innerHTML = statement.FirstName);
+        console.log(document.getElementById("ab").innerHTML = statement.LastName);
+        console.log(document.getElementById("ab").innerHTML = statement.RollNo);
+        console.log(document.getElementById("ab").innerHTML = statement.Bracnh);
+        console.log(document.getElementById("ab").innerHTML = statement.EmailID);
